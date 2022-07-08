169322e80a5c461f17aff389d8f04ae631f54d54
Laxus <laxus763@gmail.com>
2022-07-08 20:27:38 +0700

    login
    render Login UI

--- src/views/index.js
+++ src/views/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import AppViews from "./app-views";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import AppViews from './app-views';
+
 const Views = () => {
-  // const isLogin = true;
-  return (
-    <>
-      <Routes>
-        <Route path="*" element={<AppViews />} />
-      </Routes>
-    </>
-  );
+    // const isLogin = true;
+    return (
+        <React.StrictMode>
+            <Routes>
+                <Route path="*" element={<AppViews />} />
+            </Routes>
+        </React.StrictMode>
+    );
 };
 
 export default Views;
