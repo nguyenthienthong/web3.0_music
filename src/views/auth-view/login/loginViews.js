171c19048b888976950ea92f2f9dd34c18236eb9
Thong <nguyenthienthong3009@gmail.com>
2022-07-09 10:27:44 +0700

    update file

---
+++ src/views/auth-view/login/loginViews.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginViews = () => {
+  return (
+    <div className="form-login">
+      <h2>Đăng nhập</h2>
+      <p>Đăng nhập bằng tài khỏa có sẵn</p>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoginViews;
--- src/views/index.js
+++ src/views/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AppViews from "./app-views";
+import { BrowserRouter } from "react-router-dom";
+
 const Views = () => {
   // const isLogin = true;
   return (
-    <>
+    <BrowserRouter>
       <Routes>
         <Route path="*" element={<AppViews />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
