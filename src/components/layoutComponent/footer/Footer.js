614e7a1c0e3d2cb275b9f9cbdefce22e61d7edf6
dagkien <kiendang712002@gmail.com>
2022-07-11 20:21:15 +0700

    edit layout

--- src/components/layoutComponent/footer/Footer.js
+++ src/components/layoutComponent/footer/Footer.js
@@ -1,42 +1,42 @@
 import React from "react";
 import "./_footer.scss";
 const Footer = () => {
-  let data = [
-    {
-      id: 1,
-      name: "Top CV",
-    },
-    {
-      id: 2,
-      name: "Đối tác",
-    },
-    {
-      id: 3,
-      name: "Hồ sơ và CV",
-    },
-    {
-      id: 4,
-      name: "Hồ sơ và CV",
-    },
-  ];
+	let data = [
+		{
+			id: 1,
+			name: "Top CV",
+		},
+		{
+			id: 2,
+			name: "Đối tác",
+		},
+		{
+			id: 3,
+			name: "Hồ sơ và CV",
+		},
+		{
+			id: 4,
+			name: "Hồ sơ và CV",
+		},
+	];
 
-  return (
-    <div className="footer">
-      {data.map((e) => {
-        return (
-          <div key={e.id} className="footer__item" span={6}>
-            <h2>{e.name}</h2>
-            <ul>
-              <li>Giới thiêụ</li>
-              <li>Góc báo chí</li>
-              <li>Tuyển dụng</li>
-              <li>Liên hệ</li>
-            </ul>
-          </div>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className="footer">
+			{data.map((e) => {
+				return (
+					<div key={e.id} className="footer__item" span={6}>
+						<h2>{e.name}</h2>
+						<ul>
+							<li>Giới thiêụ</li>
+							<li>Góc báo chí</li>
+							<li>Tuyển dụng</li>
+							<li>Liên hệ</li>
+						</ul>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Footer;
--- src/views/app-views/index.js
+++ src/views/app-views/index.js
@@ -1,24 +1,28 @@
 import { Layout } from "antd";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Footer from "../../components/layoutComponent/footer/Footer";
-import Header from "../../components/layoutComponent/header/Header";
+import Footer from "../../components/layoutComponent/Footer/Footer";
+import Header from "../../components/layoutComponent/Header/Header";
+import Sidebar from "../../components/layoutComponent/Sidebar/Sidebar";
 import NotFound from "../notFound/404";
 import Home from "./Home";
 const { Content } = Layout;
 const AppViews = () => {
-  return (
-    <Layout>
-      <Header />
-      <Content id="container">
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="*" element={<NotFound/>}></Route>
-          </Routes>
-      </Content>
-      <Footer />
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Header />
+			<main id="main_container">
+				<Sidebar />
+				<Content id="container">
+					<Routes>
+						<Route exact path="/" element={<Home />}></Route>
+						<Route path="*" element={<NotFound />}></Route>
+					</Routes>
+				</Content>
+			</main>
+			{/* <Footer /> */}
+		</Layout>
+	);
 };
 
 export default AppViews;
