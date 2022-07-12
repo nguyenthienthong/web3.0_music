b1399562fff758814322f4017602b728fe3f5d7d
dagkien <kiendang712002@gmail.com>
2022-07-12 09:28:48 +0700

    setup

--- src/views/app-views/index.js
+++ src/views/app-views/index.js
@@ -1,24 +1,24 @@
 import { Layout } from "antd";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Footer from "../../components/layoutComponent/footer/Footer";
-import Header from "../../components/layoutComponent/header/Header";
+import Footer from "../../components/layoutComponent/Footer/Footer";
+import Header from "../../components/layoutComponent/Header/Header";
 import NotFound from "../notFound/404";
 import Home from "./Home";
 const { Content } = Layout;
 const AppViews = () => {
-  return (
-    <Layout>
-      <Header />
-      <Content id="container">
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="*" element={<NotFound/>}></Route>
-          </Routes>
-      </Content>
-      <Footer />
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Header />
+			<Content id="container">
+				<Routes>
+					<Route exact path="/" element={<Home />}></Route>
+					<Route path="*" element={<NotFound />}></Route>
+				</Routes>
+			</Content>
+			<Footer />
+		</Layout>
+	);
 };
 
 export default AppViews;
