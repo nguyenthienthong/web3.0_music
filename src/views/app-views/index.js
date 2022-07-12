f683fb206670c312d8e02aa7c56113998d8a3c19
dagkien <kiendang712002@gmail.com>
2022-07-12 09:33:29 +0700

    a

--- src/views/app-views/index.js
+++ src/views/app-views/index.js
@@ -3,10 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Footer from "../../components/layoutComponent/Footer/Footer";
 import Header from "../../components/layoutComponent/Header/Header";
-<<<<<<< HEAD
 import Sidebar from "../../components/layoutComponent/Sidebar/Sidebar";
-=======
->>>>>>> b1399562fff758814322f4017602b728fe3f5d7d
 import NotFound from "../notFound/404";
 import Home from "./Home";
 const { Content } = Layout;
